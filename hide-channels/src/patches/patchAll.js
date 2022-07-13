c6c3c2293ae8a306849b08d6019d769262ca545b
Kıraç Armağan Önal <kiracarmaganonal@gmail.com>
2022-07-13 05:10:21 +0300

    fix scroll margin

--- hide-channels/src/patches/patchAll.js
+++ hide-channels/src/patches/patchAll.js
@@ -4,12 +4,12 @@ import patcher from "@cumcord/patcher";
 import { dataStore, GuildChannelStore, GuildReadStateStore } from "../other/apis";
 
 import webpack from "@cumcord/modules/webpack";
+import { findComponentByNameAndSelector } from "../utils";
 
 export async function patchAll() {
-
-  const ChannelItem = webpack.find(i => i?.default?.displayName == "ChannelItem");
-
+  
   const VoiceUsers = webpack.find(i => i?.default?.displayName == "VoiceUsers");
+  const ChannelItem = webpack.find(i => i?.default?.displayName == "ChannelItem");
 
   const voiceUsersPatch = patcher.instead("render", VoiceUsers.default.prototype, function(args, ogMethod) {
     let hidden = [...dataStore.hiddenChannels];
@@ -54,4 +54,48 @@ export async function patchAll() {
     hasUnreadPatch,
     windowAPIPatch
   );
+
+  (async () => {
+    let VoiceChannel = await findComponentByNameAndSelector("VoiceChannel", '[class*="containerDefault-"]');
+
+    const voiceChannelPatch = patcher.instead("render", VoiceChannel.prototype, function(args, ogMethod) {
+      let hidden = [...dataStore.hiddenChannels];
+      if (hidden.includes(this?.props?.channel?.id)) {
+        return null;
+      }
+      return ogMethod.call(this, ...args);
+    });
+
+    patchContainer.add(voiceChannelPatch);
+  })();
+
+  (async () => {
+    let Channels = await findComponentByNameAndSelector("Channels", '#channels');
+
+    const voiceChannelPatch = patcher.instead("render", Channels.prototype, function(args, ogMethod) {
+      let hidden = [...dataStore.hiddenChannels];
+      let hiddenLength = hidden.length;
+      let res = ogMethod.call(this, ...args);
+      if (this?.props?.voiceStates) {
+        for (let i = 0; i < hiddenLength; i++) {
+          const id = hidden[i];
+          delete this.props.voiceStates[id];
+        }
+      }
+      if (this?.props?.guildChannels?.sortedNamedCategories) {
+        let cats = this?.props?.guildChannels?.sortedNamedCategories;
+        let catsLength = cats.length;
+        for (let i = 0; i < catsLength; i++) {
+          const cat = cats[i];
+          for (let j = 0; j < hiddenLength; j++) {
+            const id = hidden[j];
+            delete cat.channels[id];
+          }
+        }
+      }
+      return res;
+    });
+
+    patchContainer.add(voiceChannelPatch);
+  })();
 }
--- hide-channels/src/utils/index.js
+++ hide-channels/src/utils/index.js
@@ -1,4 +1,6 @@
-export function arrayToggler(arrGetter=()=>{}, afterChange=()=>{}) {
+import utils from "@cumcord/utils";
+
+export function arrayToggler(arrGetter = () => { }, afterChange = () => { }) {
   return (value) => {
     let arr = arrGetter();
     if (arr.includes(value)) {
@@ -9,4 +11,45 @@ export function arrayToggler(arrGetter=()=>{}, afterChange=()=>{}) {
     afterChange(arr);
     return arr;
   }
+}
+
+const cachedComponents = new Map();
+
+export function getComponentsByNode(node) {
+  const instance = utils.getReactInstance(node);
+  const components = [];
+  let lastInstance = instance;
+  while (lastInstance && lastInstance.return) {
+    if (typeof lastInstance.return.type === "string") break;
+    if (lastInstance.return.type) components.push(lastInstance.return.type);
+    lastInstance = lastInstance.return;
+  }
+  return components;
+}
+
+export async function sleep(ms = 0) { return new Promise(r => setTimeout(r, ms)) };
+
+export async function findComponentByNameAndSelector(name, selector) {
+  if (cachedComponents.has(name)) return cachedComponents.get(name);
+
+  let component;
+
+  while (true) {
+    const elements = document.querySelectorAll(selector);
+    if (!elements.length) {
+      await sleep(100);
+      continue;
+    }
+
+    for (const element of elements) {
+      const componentsFound = getComponentsByNode(element);
+      component = componentsFound.find(i => i?.displayName == name);
+      if (component) break;
+    }
+
+    if (component) break;
+  }
+
+  cachedComponents.set(name, component);
+  return component;
 }
