52c306ee6ee7872a9a7dd3e05c140920081af282
Kıraç Armağan Önal <kiracarmaganonal@gmail.com>
2022-07-17 00:50:32 +0300

    performance

--- voice-indicators/src/other/api.js
+++ voice-indicators/src/other/api.js
@@ -1,8 +1,24 @@
 import { awaitResponse } from "../connection/websocket";
 import { getUserVoiceStateShaped } from "./VoiceStates";
 
+const cache = new Map();
+
+setInterval(() => {
+  cache.forEach((item, key) => {
+    if (Date.now() - item.at > 1000) {
+      cache.delete(key);
+    }
+  });
+}, 1000);
+
 export async function fetchUserVoiceStates(userId) {
   let state = getUserVoiceStateShaped(userId);
-  if (!state) state = (await awaitResponse("voiceState", userId))?.data;
+  if (!state) {
+    let cached = cache.get(userId);
+    if (cached && !(Date.now() - cached.at > 1000)) return cached.state;
+
+    state = (await awaitResponse("voiceState", userId))?.data;
+    cache.set(userId, { at: Date.now(), state });
+  }
   return state;
 }
